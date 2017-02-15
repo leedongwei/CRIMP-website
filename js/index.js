2898de127fd3a45bf2f53dea534def1a14bdde3a
DongWei <leedongwei@gmail.com>
2017-02-15 20:46:19 +0800

    Added Google Webmaster verification

--- js/index.js
+++ js/index.js
@@ -3,18 +3,6 @@ $('#open-ui-sidebar').on('click', () => {
 });
 
 
-// Facebook share button on header
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4&appId=502179363281634";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-
-
-
 // Google Analytics
 /*
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
