3a83dd1af6d79168559af8853597254bfa30bf98
DongWei <leedongwei@gmail.com>
2017-02-17 21:11:27 +0800

    Minor changes, added new assets

--- js/index.js
+++ js/index.js
@@ -3,20 +3,20 @@ $('#open-ui-sidebar').on('click', () => {
 });
 
 
-// Google Analytics
-/*
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-91870246-1', 'auto');
-ga('send', 'pageview');
-*/
 
 
-// Drift
+
 if (window.location.hostname == 'crimp.rocks') {
+  // Google Analytics
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-91870246-1', 'auto');
+  ga('send', 'pageview');
+
+
+  // Drift
   !function() {
     var t;
     if (t = window.driftt = window.drift = window.driftt || [], !t.init) return t.invoked ? void (window.console && console.error && console.error("Drift snippet included twice.")) : (t.invoked = !0,
